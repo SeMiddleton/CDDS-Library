44c9ebe81560b5512ae459762727ff4f9fe8ee65
Sam <semiddleton1@live.com>
2023-01-23 10:16:53 -0600

    Initial Creation and push

---
+++ CDDLibrary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int loopIteration;
+bool swapped;
+int list[];
+
+
+BubbleSort(list)
+{
+	loopIteration = list.count;
+
+	for i = 0->loopIteration
+}
+
+
+
+int main
+{
+
+
+
+
+	return 0;
+}
